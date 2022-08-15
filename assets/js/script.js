4ef5de42208a41dfdc84fdd856162cdb10f4d706
Colin Shaw <colin.shaw400@outlook.com>
2022-08-15 17:28:55 +0000

    Add display status function to get API  heading, results and title

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,19 @@ async function getStatus(e){
     const data = await response.json();
 
     if(response.ok){
-        console.log(data);
+        displayStatus(data);
+    }else {
+        throw new Error(data.error);
     }
+}
+//display status function  to set heading text and body text for valid key
+function displayStatus(data){
+    let heading = "API Key Status";
+    let results = `<div>Your Key is valid until</div>`
+    results += `<div class="key-status">${data.expiry}</div<`
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
 }
