b674899c2b02e002ded0870b928a74db5c1ff888
Colin Shaw <colin.shaw400@outlook.com>
2022-08-15 20:40:17 +0000

    Add display erros functions to display the errors not in the console but on the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,20 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-function processOptions(form){
+function processOptions(form) {
     let optArray = [];
 
-    for (let entry of form.entries()){
-        if(entry[0] === "options"){
-            optArray.push(entry[1]);
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
         }
-        form.delete("options");
-
-        form.append("options", optArray.join());
-        
-        return form;
-
     }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
 }
 
 async function postForm(e) {
@@ -39,6 +39,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -55,9 +56,23 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
+}
+
+function displayException(data) {
+
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayErrors(data) {
